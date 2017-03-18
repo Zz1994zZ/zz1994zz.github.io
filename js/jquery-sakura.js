4fef0a7318761b10b04387320df96465b215da78
zz1994 <654938874@qq.com>
2017-03-18 16:22:02 +0800

    handle sakura bug and add links

--- js/jquery-sakura.js
+++ js/jquery-sakura.js
@@ -56,8 +56,8 @@
         return (
             rect.top >= 0 &&
                 rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+                rect.bottom <= (window.innerHeight-80 || document.documentElement.clientHeight-80) &&
+                rect.right <= (window.innerWidth-80 || document.documentElement.clientWidth-80)
             );
     }
 

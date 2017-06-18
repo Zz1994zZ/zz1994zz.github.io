f1ef03ddf760dd7096e0e9ef27fb53c475ceb352
zz1994 <654938874@qq.com>
2017-06-18 23:56:34 +0800

    add moveTo

--- js/index.js
+++ js/index.js
@@ -21,41 +21,41 @@ function Role(){
 	role.x=500;
 	role.y=320;
 	role.state=0;//0静止 1移动 2攻击 3死亡
-	role.direction=3;//     0上
-//						 3左边   	1右  
-//                          2下
 	role.hp=100;
-	role.speed=1;
+	role.speed=5;
 	role.moveState=0;
 	role.imgs=new Array();
 	role.img=null;
 	role.enable=true;
-	role.angle=0;//角度
-	role.setAngle=function(x,y){
-		//console.log("未定义设置角度！~");
-	}
+	role.targetX=role.x;
+	role.targetY=role.y;
+	role.actionNum=0;
+	role.dx=0;
+	role.dy=0;
 	role.refresh=function(){
 		//console.log("未定义刷新！")
 		};
 	role.realMove=function(){
-		switch(role.direction){
-		   case 3:
-			  //role.imgState=roleLeft;
-			  role.x =  role.x -  role.speed;
-			  break;
-		   case 0:
-			   //role.imgState=roleUp;
-			   role.y =  role.y -  role.speed;
-			  break;
-		   case 1:
-			  //role.imgState=roleRight;
-			   role.x =  role.x +  role.speed;
-			  break;
-		   case 2:
-			  //role.imgState=roleDown;
-			  role.y =  role.y +  role.speed;
-			  break;
-		}
+		 role.x =  role.x +  role.dx;
+		 role.y =  role.y +  role.dy;
+	}
+	role.setAngle=function(x,y){
+		var a=y-this.y;
+		var b=x-this.x;
+		var angle=Math.atan(a/b);
+		this.dx=this.speed*Math.cos(angle)*(b>0? 1:-1);
+		this.dy=Math.abs(this.speed*Math.sin(angle))*(a>0? 1:-1);
+	}
+	role.moveTo=function(x,y){
+		role.targetX=x;
+		role.targetY=y;
+		var a=x-this.x;
+		var b=y-this.y;
+		var lengh=Math.sqrt(a*a+b*b);
+		this.actionNum=lengh/this.speed;
+		this.setAngle(x,y);
+		this.state=1;
+		//
 	}
 	role.canvas=document.createElement('canvas');
 	role.canvas.width=100; 
@@ -91,12 +91,19 @@ function alien(){
 				this.moveState++;
 				break;
 			case 1://运动
-				if(this.moveState>=3*anum)
-					this.moveState=0;
-				if(this.moveState%anum==0)
-				this.img=this.imgs[this.moveState/anum];
-				this.moveState++;
-				this.realMove();
+				if(this.actionNum>0){
+					if(this.moveState>=3*anum)
+						this.moveState=0;
+					if(this.moveState%anum==0)
+					this.img=this.imgs[this.moveState/anum];
+					this.moveState++;
+					this.realMove();
+					this.actionNum--;
+				}else{
+					this.x=this.targetX;
+					this.y=this.targetY;
+					this.state=0;
+				}
 				break;
 			case 2://攻击
 				if(this.moveState>=8){	
@@ -114,26 +121,8 @@ function alien(){
 			this.cxt.drawImage(this.img,-5,0);
 	}
 	alien.attack=function(){
-		if(this.state!=2){
-			this.state=2;
-			this.moveState=0;
-			if(this.angle>0){
-				if(this.direction!=3)
-					this.cxt.transform(-1,0,0,1,100,0);
-				this.direction=3;
-			}
-			else {
-				if(this.direction!=1)
-					this.cxt.transform(-1,0,0,1,100,0);
-				this.direction=1;
-			}
-		}
+		
 	}
-	alien.setAngle=function(x,y){
-		this.angle=(y-this.y-50)/(x-this.x-50);
-		//console.log("monkey设置角度"+this.angle);
-	}
-	
 	return alien;
 }
 function Monkey(){
@@ -154,12 +143,20 @@ function Monkey(){
 				this.img=this.imgs[this.moveState];
 				break;
 			case 1://运动
-				if(this.moveState>=3*anum)
-					this.moveState=0;
-				if(this.moveState%anum==0)
-				this.img=this.imgs[this.moveState/anum];
-				this.moveState++;
-				this.realMove();
+				if(this.actionNum>0){
+					if(this.moveState>=3*anum)
+						this.moveState=0;
+					if(this.moveState%anum==0)
+					this.img=this.imgs[this.moveState/anum];
+					this.moveState++;
+					this.realMove();
+					this.actionNum--;
+				}else{
+					this.x=this.targetX;
+					this.y=this.targetY;
+					this.state=0;
+				}
+					
 				break;
 			case 2://攻击
 				if(this.moveState>=8*anum){	
@@ -178,10 +175,11 @@ function Monkey(){
 			this.cxt.drawImage(this.img,20,20);
 	}
 	monkey.attack=function(){
+		
 		if(this.state!=2){
 			this.state=2;
 			this.moveState=0;
-			if(this.angle>0){
+			/*if(this.angle>0){
 				if(this.direction!=3)
 					this.cxt.transform(-1,0,0,1,100,0);
 				this.direction=3;
@@ -190,14 +188,11 @@ function Monkey(){
 				if(this.direction!=1)
 					this.cxt.transform(-1,0,0,1,100,0);
 				this.direction=1;
-			}
+			}*/
 		}
 		
 	}
-	monkey.setAngle=function(x,y){
-		this.angle=(y-this.y-50)/(x-this.x-50);
-		//console.log("monkey设置角度"+this.angle);
-	}
+
 	
 	return monkey;
 }
@@ -209,12 +204,17 @@ function Banana(monkey){
 	img.src="img/monkey/"+i+".png";
 	banana.imgs.push(img);
 	}
-	banana.realMove=function(){
-		var a=this.angle>0 ? 1:-1;
-		this.x =  this.x +  this.speed*(-a);
-		this.y =  this.y +  this.speed*this.angle*(-a);
-		
-	}
+	banana.state=1;
+	banana.speed=anum;
+	banana.y=monkey.y;
+	banana.x=monkey.dx<0? monkey.x-50:monkey.x+50;
+	banana.dx=monkey.dx;
+	banana.dy=monkey.dy;	
+	
+	if(monkey.dx>0)
+		banana.cxt.transform(-1,0,0,1,100-1,0);
+	//banana.cxt.transform(Math.cos(banana.angle*Math.PI/180),Math.sin(banana.angle*Math.PI/180),-Math.sin(banana.angle*Math.PI/180),Math.cos(banana.angle*Math.PI/180),0,0)
+	//banana.cxt.transform(0.70710678118655,0.70710678118655,-0.70710678118655,0.70710678118655,0,0);
 	banana.refresh=function(){
 			
 			switch(this.state){
@@ -244,16 +244,6 @@ function Banana(monkey){
 				this.cxt.drawImage(this.img,50-this.img.width/2,50-this.img.height/2);
 				
 	}
-	banana.direction=monkey.direction;
-	banana.state=1;
-	banana.speed=anum;
-	banana.y=monkey.y;
-	banana.x=monkey.direction==3? monkey.x-50:monkey.x+50;
-	banana.angle=monkey.angle;
-	if(banana.direction==1)
-		banana.cxt.transform(-1,0,0,1,100-1,0);
-	//banana.cxt.transform(Math.cos(banana.angle*Math.PI/180),Math.sin(banana.angle*Math.PI/180),-Math.sin(banana.angle*Math.PI/180),Math.cos(banana.angle*Math.PI/180),0,0)
-	//banana.cxt.transform(0.70710678118655,0.70710678118655,-0.70710678118655,0.70710678118655,0,0);
 	return banana;
 }
 function isTouch(a,b){
@@ -286,18 +276,22 @@ function init(){
 	alien=new alien();
 	roleList.push(monkey);
 	roleList.push(alien);
-	canvas.onmousemove=function(e){
-		var x=e.clientX;
-		var y=e.clientY;
-		for (var i = 0; i <roleList.length; i++) {
-			roleList[i].setAngle(x,y);
-		}
-		//roleList.push(new Banana());
-		monkey.attack();
-	}
+
 	zzsakura=new Image();
 	zzsakura.src="img/sakura.png";
 	
+		
+	canvas.onmousemove=function(e){
+		var x=e.clientX-100;
+		var y=e.clientY-70;
+		//for (var i = 0; i <roleList.length; i++) {
+		//	roleList[i].moveTo(x,y);
+		//}
+		//roleList.push(new Banana());
+		//monkey.attack();
+		alien.moveTo(x,y);
+		monkey.setAngle(x,y);
+	}
 }
 function draw(){
 	var cxt=canvas.getContext("2d");
@@ -324,73 +318,21 @@ function start(){
 	init();
 	window.setInterval(main,16);
 }
-function move(code,role){
-		role.state=1;
-	    switch(code)
-	    {
-		   case 37:
-			  if(role.direction==1)
-				role.cxt.transform(-1,0,0,1,100,0);//TODO无法记录
-		      role.direction=3;
-			  break;
-		   case 38:
-		      role.direction=0;
-			  break;
-		   case 39:
-		    if(role.direction==3)
-				role.cxt.transform(-1,0,0,1,100,0);
-		     role.direction=1;
-			  break;
-		   case 40:
-		      role.direction=2;
-			  break;
-		} 
-}
+
 window.onkeydown=function(event){
    var code = event.keyCode;
-   //if(code >=37&&code<= 40){
-   //   move(code,monkey);
-	if(code ==37 || code== 39){
-      move(code,monkey);
-   }else if(code ==32){
-	  monkey.attack();
-   }else{
-	   switch(code){
-			case 87:
-		     move(38,alien);
-			break;
-			case 83:
-		     move(40,alien);
-			break;
-			case 65:
-		     move(37,alien);
-			break;
-			case 68:
-		     move(39,alien);
-			break;
-	   }
+   switch(code){
+	case 37:
+		monkey.moveTo(monkey.x-monkey.speed,monkey.y);
+		break;
+	case 39:
+		monkey.moveTo(monkey.x+monkey.speed,monkey.y);
+		break;
+	case 32:
+		monkey.attack();
+		break;
+		
    }
  }
-function stop(code,role){
-	var ct=-1;
-	    switch(code)
-	    {
-		   case 37:
-		     ct=3;
-			  break;
-		   case 38:
-		      ct=0;
-			  break;
-		   case 39:
-		     ct=1;
-			  break;
-		   case 40:
-		     ct=2;
-			  break;
-		} 
-		if(role.direction==ct){
-			role.state=0;
-			role.moveState=0;		
-		}
-}
+
 window.onload=start;
